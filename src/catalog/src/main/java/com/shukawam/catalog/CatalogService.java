4d27fce70a0fe2da7779559762dc016a9bec4e15
shukawam <shukawam@gmail.com>
2022-06-27 07:39:20 +0000

    Fixed implement

--- src/catalog/src/main/java/com/shukawam/catalog/CatalogService.java
+++ src/catalog/src/main/java/com/shukawam/catalog/CatalogService.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class CatalogService {
-    private final List<Catalog> catalogList = new ArrayList<>();
+    @ConfigProperty(name = "app.imageUrl")
+    private String isImageUrl;
 
-    @Inject
-    public CatalogService(@ConfigProperty(name = "app.imageUrl") String isImageUrl) {
-        System.out.println(Boolean.parseBoolean(isImageUrl));
+    public List<Catalog> getCatalogList() {
+        var catalogList = new ArrayList<Catalog>();
         if (Boolean.parseBoolean(isImageUrl)) {
             catalogList.add(new Catalog(1, "amber", Department.Eyewear, "Elinor Glasses", 282.00, "http://lorempixel.com/640/480"));
             catalogList.add(new Catalog(2, "cyan", Department.Clothing, "Atlas Shirt", 127.00, "http://lorempixel.com/640/480"));
@@ -26,10 +25,6 @@ public class CatalogService {
             catalogList.add(new Catalog(3, "teal", Department.Clothing, "Small Metal Shoes", 232.00));
             catalogList.add(new Catalog(4, "red", Department.Watches, "Red Dragon Watch", 232.00));
         }
-        
-    }
-
-    public List<Catalog> getCatalogList() {
         return catalogList;
     }
 
